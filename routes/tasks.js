72571532a4a5e487d1a6bc075313dffecf178bca
tgotwig <tgotwig@outlook.de>
2019-10-26 11:38:24 +0200

    test

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
+var fs = require('fs');
 //var db = mongojs('mongodb://brad:brad@ds047666.mlab.com:47666/mytasklist_brad', ['tasks']);
 //var db = mongojs('mongodb://admin:123ali@ds257640.mlab.com:57640/creating-node-api', ['tasks']);
 // We are adding Docker-compose.yaml file and will create a underlying layer of docker with mongo so no need to connect URL of MongoDB
-const mongo_connection = (process.env.mongo_connection)
-    ? process.env.mongo_connection : 'mongodb://mongo:27017'
+const mongo_ip = process.env.mongo_ip
+const mongo_user = process.env.mongo_user
+const mongo_pw = process.env.mongo_pw
+const mongo_connection = `mongodb://${mongo_user}:${mongo_pw}@${mongo_ip}`
 var db = mongojs(mongo_connection, ['tasks']);
-
-
+// var db = mongojs("mongodb://user:pw@mongo-service", ['tasks']);
 
 // Get All Tasks
 router.get('/tasks', function(req, res, next){
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+user=user \
+pw=pw \
+mongo_connection=mongodb://user:pw@localhost \
+    npm start
